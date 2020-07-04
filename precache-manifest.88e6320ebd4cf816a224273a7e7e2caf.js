1417897b3139bd22cd921dc9488834d3acd3f362
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-07-04 20:37:53 +0530

    Updates

---
+++ precache-manifest.88e6320ebd4cf816a224273a7e7e2caf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7274c4a46034c30a9297479a49c73c04",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "00a36794ca7680d4d712",
+    "url": "/monsters-rolodex/static/css/main.37a83df4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a36794ca7680d4d712",
+    "url": "/monsters-rolodex/static/js/main.8d462a10.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
